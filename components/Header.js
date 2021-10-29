fac4ae51c7511ed940cd337db9d66ea8d4e79238
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-29 06:34:26 -0400

    logo

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,37 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 function Header() {
+	const [navbar, setNavbar] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener('scroll', changeHeader);
+		return () => window.removeEventListener('scroll', changeHeader);
+	});
+
+	const changeHeader = () => {
+		if (window.scrollY >= 80) {
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	};
 	return (
-		<header className="z-10 fixed top-0 flex w-full p-4 text-white">
-			<div className="text-4xl flex-grow">AC</div>
-			<div className="flex text-xl gap-4">
-				<button>SKILLS</button>
-				<button>PROJECTS</button>
-				<button>CONTACT</button>
+		<header
+			className={
+				navbar
+					? 'z-10 fixed bg-black top-0 flex w-full p-4 text-white font-light'
+					: 'z-10 fixed top-0 flex w-full p-4 text-white font-light'
+			}
+		>
+			<div className="flex-grow">
+				<button>
+					<Image src="/icons/AC.png" height={72} width={72} />
+				</button>
+			</div>
+			<div className="flex text-xl gap-6">
+				<button className="font-light">SKILLS</button>
+				<button className="font-light">PROJECTS</button>
+				<button className="font-light">CONTACT</button>
 			</div>
 		</header>
 	);
--- components/Hero.js
+++ components/Hero.js
@@ -13,8 +13,16 @@ function Hero() {
 			<div className="absolute inset-0 w-screen  bg-black opacity-40" />
 			<Header />
 			<section className="w-full flex flex-col items-center justify-center absolute top-1/2">
-				<h4 className="text-white text-3xl font-light">HI, I'M ANTHONY</h4>
-				<h1 className="text-white text-6xl font-light">WEB DEVELOPER</h1>
+				<h4 className="text-gray-100 text-2xl font-thin uppercase">
+					Hi, I'm Anthony
+				</h4>
+				<h1
+					className="text-white text-5xl font-thin
+					
+				 uppercase"
+				>
+					Frontend Developer
+				</h1>
 			</section>
 		</div>
 	);
