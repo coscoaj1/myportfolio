1299eb8d0a72a5b67a05119fae7e8e4a2d3df48d
coscoaj1 <coscoanthony1@gmail.com>
2021-10-31 17:56:57 -0400

    added formik

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function Contact() {
   const [name, setName] = useState("");
@@ -6,6 +8,17 @@ function Contact() {
   const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Must be at least 3 characters")
+      .required("Name required."),
+    email: Yup.string()
+      .required("Email required")
+      .min(6, "Must be at least 6 characters"),
+    message: Yup.string()
+      .required("Message required")
+      .min(12, "Must be at least 12 characters"),
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("sending");
@@ -43,82 +56,118 @@ function Contact() {
             <div>
               <h1 className="text-2xl font-semibold">Contact Form</h1>
             </div>
-            <div className="divide-y divide-gray-200">
-              <form className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div className="relative">
-                  <input
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                    autoComplete="off"
-                    id="name"
-                    name="name"
-                    type="name"
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Name"
-                    value={name}
-                  />
-                  <label
-                    htmlFor="name"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Full Name
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    autoComplete="off"
-                    id="email"
-                    name="email"
-                    type="text"
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Email address"
-                    value={email}
-                  />
-                  <label
-                    htmlFor="email"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Email Address
-                  </label>
-                </div>
-                <div className="relative h-30">
-                  <textarea
-                    onChange={(e) => {
-                      setMessage(e.target.value);
-                    }}
-                    autoComplete="off"
-                    cols="40"
-                    rows="3"
-                    id="message"
-                    name="message"
-                    type="message"
-                    placeholder="Message"
-                    value={message}
-                    className="peer placeholder-transparent h-30 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                  ></textarea>
-                  <label
-                    htmlFor="message"
-                    className="absolute pb-2 left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Message:
-                  </label>
-                </div>
-                <div className="relative">
-                  <button
-                    type="submit"
-                    onClick={(e) => {
-                      handleSubmit(e);
-                    }}
-                    className="bg-[#0891b2] text-white rounded-md px-2 py-1 mt-2 transition transform active:scale-90 duration-150"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+            <div className="divide-y divide-gray-200 w-full h-auto">
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  message: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                  console.log(values);
+                  fetch(`/api/contact`, {
+                    method: "POST",
+                    headers: {
+                      Accept: "application/json, text/plain, *.*",
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(values),
+                  }).then((res) => {
+                    console.log("response received");
+                    if (response === 200) {
+                      setSubmitted(true);
+                      setName("");
+                      setEmail("");
+                      setBody("");
+                    }
+                  });
+                }}
+              >
+                <Form className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                  <div className="relative">
+                    <Field
+                      autoComplete="off"
+                      id="name"
+                      name="name"
+                      type="name"
+                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      placeholder="Name"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      render={(msg) => (
+                        <div className="text-red-500">{msg}</div>
+                      )}
+                    />
+                    <label
+                      htmlFor="name"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Full Name
+                    </label>
+                  </div>
+                  <div className="relative">
+                    <Field
+                      autoComplete="off"
+                      id="email"
+                      name="email"
+                      type="email"
+                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      placeholder="Email address"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      render={(msg) => (
+                        <div className="text-red-500">{msg}</div>
+                      )}
+                    />
+
+                    <label
+                      htmlFor="email"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Email Address
+                    </label>
+                  </div>
+                  <div className="relative h-30">
+                    <Field
+                      as="textarea"
+                      autoComplete="off"
+                      cols="40"
+                      rows="3"
+                      id="message"
+                      name="message"
+                      type="message"
+                      placeholder="Message"
+                      className="peer placeholder-transparent h-30 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    />
+                    <ErrorMessage
+                      name="message"
+                      render={(msg) => (
+                        <div className="text-red-500">{msg}</div>
+                      )}
+                    />
+                    <label
+                      htmlFor="message"
+                      className="absolute pb-2 left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Message:
+                    </label>
+                  </div>
+                  <div className="relative">
+                    <button
+                      type="submit"
+                      onClick={(values) => {
+                        console.log(values);
+                      }}
+                      className="bg-[#0891b2] text-white rounded-md px-2 py-1 mt-2 transition transform active:scale-90 duration-150"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </Form>
+              </Formik>
             </div>
           </div>
         </div>
