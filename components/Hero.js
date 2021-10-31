11c8a54c6457818e0ebd313a19a16a52939b9d8e
Anthony Cosco <coscoanthony1@gmail.com>
2021-10-31 07:59:51 -0400

    mobile friendly changes

--- components/Hero.js
+++ components/Hero.js
@@ -9,12 +9,14 @@ function Hero() {
 			<div id="wrapper">
 				<section className="text-white w-full flex flex-col absolute top-1/2">
 					<div className="flex flex-col gap-2 sm:gap-5 sm:pl-4 font-bold text-[#fafafa]">
-						<div className="font-light text-xl">Hi, I'm Anthony</div>
-						<div className=" flex flex-row text-4xl sm:text-7xl">
+						<div className="font-light text-xl text-center sm:text-left">
+							Hi, I'm Anthony
+						</div>
+						<div className=" flex flex-row text-4xl sm:text-7xl justify-center sm:justify-start">
 							<div>Frontend </div>{' '}
 							<div className="text-[#818cf8]">Developer</div>
 						</div>
-						<div className="italic text-3xl my-2 font-medium">
+						<div className="italic text-3xl my-2 font-medium text-center sm:text-left">
 							This is my porfolio
 						</div>
 					</div>
