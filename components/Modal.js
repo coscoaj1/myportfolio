451caac6521c5886d3e0610e78622a1c5fcb1bb8
coscoaj1 <coscoanthony1@gmail.com>
2021-11-01 13:28:57 -0400

    modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,34 +1,40 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import { IoCloseOutline } from "react-icons/io5";
 
 function Modal({ show, onClose, children, title }) {
-	const [isBrowser, setIsBrowser] = useState(false);
+  const [isBrowser, setIsBrowser] = useState(false);
 
-	useEffect(() => {
-		setIsBrowser(true);
-	}, []);
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
 
-	const handleCloseClick = (e) => {
-		e.preventDefault();
-		onClose();
-	};
+  const handleCloseClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
 
-	const modalContent = show ? (
-		<div className="w-[800px] h-[500px] z-50 border-2 absolute top-1/2 left-48 border-gray-700 bg-white">
-			<a href="#" onClick={handleCloseClick}>
-				x
-			</a>
-		</div>
-	) : null;
+  const modalContent = show ? (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-20 text-opacity-100 backdrop-filter backdrop-blur-xl z-50">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <IoCloseOutline
+          className="bg-white cursor-pointer rounded-full h-8 w-8"
+          arial-label="Close modal"
+          onClick={handleCloseClick}
+        />
+        <div className="w-[800px] h-[500px] rounded-3xl bg-white mx-0 my-auto"></div>
+      </div>
+    </div>
+  ) : null;
 
-	if (isBrowser) {
-		return ReactDOM.createPortal(
-			modalContent,
-			document.getElementById('modal-root')
-		);
-	} else {
-		return null;
-	}
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Modal;
--- components/Projects.js
+++ components/Projects.js
@@ -1,61 +1,61 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import Modal from './Modal';
+import React, { useState } from "react";
+import Image from "next/image";
+import Modal from "./Modal";
 
-function Projects() {
-	const [showModal, setShowModal] = useState(false);
+function Projects({ show, onClose, children, title, handleClick }) {
+  const [showModal, setShowModal] = useState(false);
 
-	return (
-		<div className="max-w-screen bg-[#fafafa] ">
-			<p className="mt-3 pt-3 text-2xl text-center text-gray-400 font-medium bg-[#fafafa]">
-				projects
-			</p>
-			<section className="flex flex-col justify-center items-center sm:h-auto w-screen">
-				<div className="p-6 mt-6 flex flex-col sm:flex-row items-center justify-around gap-12 bg-[#fafafa] border-b border-gray-400 w-full h-auto">
-					<a target="_blank" href="https://airbnbnextclone.vercel.app">
-						<button>
-							<Image src="/images/91374.jpg" width={600} height={500} />
-						</button>
-					</a>
-					<div className="flex flex-col gap-5 w-96 bg-[#fafafa] ">
-						<div className="text-3xl font-bold text-center">
-							{' '}
-							Full Stack Airbnb Clone
-						</div>
-						<div className="text-gray-400 text-center font-medium">
-							Created with React/Next.js, Node.js, and MongoDB
-						</div>
-						<div className="flex justify-center gap-8">
-							{' '}
-							<button
-								onClick={() => setShowModal(true)}
-								className="flex justify-center items-center w-32 px-5 bg-[#818cf8] transition duration-300 hover:bg-purple-500 active:scale-90 transform text-white font-medium rounded-3xl"
-							>
-								<Image src="/icons/preview-65 (1).svg" width={36} height={36} />
-								<span>Preview</span>
-							</button>
-							{/* <Modal onClose={() => setShowModal(false)} show={showModal}>
+  return (
+    <div className="max-w-screen bg-[#fafafa] ">
+      <p className="mt-3 pt-3 text-2xl text-center text-gray-400 font-medium bg-[#fafafa]">
+        projects
+      </p>
+      <section className="flex flex-col justify-center items-center sm:h-auto w-screen">
+        <div className="p-6 mt-6 flex flex-col sm:flex-row items-center justify-around gap-12 bg-[#fafafa] border-b border-gray-400 w-full h-auto">
+          <a target="_blank" href="https://airbnbnextclone.vercel.app">
+            <button>
+              <Image src="/images/91374.jpg" width={600} height={500} />
+            </button>
+          </a>
+          <div className="flex flex-col gap-5 w-96 bg-[#fafafa] ">
+            <div className="text-3xl font-bold text-center">
+              {" "}
+              Full Stack Airbnb Clone
+            </div>
+            <div className="text-gray-400 text-center font-medium">
+              Created with React/Next.js, Node.js, and MongoDB
+            </div>
+            <div className="flex justify-center gap-8">
+              {" "}
+              <button
+                onClick={handleClick}
+                className="flex justify-center items-center w-32 px-5 bg-[#818cf8] transition duration-300 hover:bg-purple-500 active:scale-90 transform text-white font-medium rounded-3xl"
+              >
+                <Image src="/icons/preview-65 (1).svg" width={36} height={36} />
+                <span>Preview</span>
+              </button>
+              {/* <Modal onClose={() => setShowModal(false)} show={showModal}>
 								Hello from the modal!
 							</Modal> */}
-							<a
-								href="https://github.com/coscoaj1/airbnbnextclone"
-								target="_blank"
-							>
-								<button className="flex justify-center items-center gap-1 w-32 px-5 py-1.5 border border-[#818cf8]  text-[#818cf8]  transition duration-300 active:scale-90 transform hover:scale-105 rounded-3xl">
-									<Image
-										src="/icons/iconmonstr-github-1 (1).svg"
-										width={36}
-										height={36}
-									/>
-									GitHub
-								</button>
-							</a>
-						</div>
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+              <a
+                href="https://github.com/coscoaj1/airbnbnextclone"
+                target="_blank"
+              >
+                <button className="flex justify-center items-center gap-1 w-32 px-5 py-1.5 border border-[#818cf8]  text-[#818cf8]  transition duration-300 active:scale-90 transform hover:scale-105 rounded-3xl">
+                  <Image
+                    src="/icons/iconmonstr-github-1 (1).svg"
+                    width={36}
+                    height={36}
+                  />
+                  GitHub
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Projects;
