f6d2c70098140844dd981dc7d8b276a95f975d91
coscoaj1 <coscoanthony1@gmail.com>
2021-11-02 06:53:26 -0400

    videoplayer

---
+++ components/FirstProject.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+
+function FirstProject({ onClick }) {
+  return (
+    <div className="p-6 mt-6 flex flex-col sm:flex-row items-center justify-around gap-12 bg-[#fafafa] border-b border-gray-400 w-full h-auto">
+      <a target="_blank" href="https://airbnbnextclone.vercel.app">
+        <button>
+          <Image src="/images/91374.jpg" width={600} height={500} />
+        </button>
+      </a>
+      <div className="flex flex-col gap-5 w-96 bg-[#fafafa] ">
+        <div className="text-3xl font-bold text-center">
+          {" "}
+          Full Stack Airbnb Clone
+        </div>
+        <div className="text-gray-400 text-center font-medium">
+          Created with React/Next.js, Node.js, and MongoDB
+        </div>
+        <div className="flex justify-center gap-8">
+          {" "}
+          <button
+            onClick={onClick}
+            className="flex justify-center items-center w-32 px-5 bg-[#818cf8] transition duration-300 hover:bg-purple-500 active:scale-90 transform text-white font-medium rounded-3xl"
+          >
+            <Image src="/icons/preview-65 (1).svg" width={36} height={36} />
+            <span>Preview</span>
+          </button>
+          <a href="https://github.com/coscoaj1/airbnbnextclone" target="_blank">
+            <button className="flex justify-center items-center gap-1 w-32 px-5 py-1.5 border border-[#818cf8]  text-[#818cf8]  transition duration-300 active:scale-90 transform hover:scale-105 rounded-3xl">
+              <Image
+                src="/icons/iconmonstr-github-1 (1).svg"
+                width={36}
+                height={36}
+              />
+              GitHub
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FirstProject;
--- components/Modal.js
+++ components/Modal.js
@@ -1,43 +1,43 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
-import { IoCloseOutline } from 'react-icons/io5';
-import VideoPlayer from './VideoPlayer';
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import { IoCloseOutline } from "react-icons/io5";
+import VideoPlayer from "./VideoPlayer";
 
 function Modal({ show, onClose, children, title }) {
-	const [isBrowser, setIsBrowser] = useState(false);
+  const [isBrowser, setIsBrowser] = useState(false);
 
-	useEffect(() => {
-		setIsBrowser(true);
-	}, []);
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
 
-	const handleCloseClick = (e) => {
-		e.preventDefault();
-		onClose();
-	};
+  const handleCloseClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
 
-	const modalContent = show ? (
-		<div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-20 text-opacity-100 backdrop-filter backdrop-blur-xl z-50">
-			<div className="flex flex-col items-center justify-center h-screen my-auto">
-				<IoCloseOutline
-					className="bg-white cursor-pointer rounded-full h-8 w-8 mb-8"
-					arial-label="Close modal"
-					onClick={handleCloseClick}
-				/>
-				<div className="flex items-center justify-center w-[800px] h-[500px] rounded-3xl bg-white mx-0">
-					<VideoPlayer />
-				</div>
-			</div>
-		</div>
-	) : null;
+  const modalContent = show ? (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-20 text-opacity-100 backdrop-filter backdrop-blur-xl z-50">
+      <div className="flex flex-col items-center justify-center h-screen my-auto">
+        <IoCloseOutline
+          className="bg-white cursor-pointer rounded-full h-8 w-8 mb-8"
+          arial-label="Close modal"
+          onClick={handleCloseClick}
+        />
+        <div className="flex items-center justify-center w-[800px] h-[500px] rounded-3xl bg-white mx-0">
+          <VideoPlayer videoId="2021-11-01_16-33-54_sk2kaa" />
+        </div>
+      </div>
+    </div>
+  ) : null;
 
-	if (isBrowser) {
-		return ReactDOM.createPortal(
-			modalContent,
-			document.getElementById('modal-root')
-		);
-	} else {
-		return null;
-	}
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Modal;
