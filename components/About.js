2832c502a1cab92d80126a81e7109093a8394504
Anthony Cosco <coscoanthony1@gmail.com>
2021-11-04 07:03:52 -0400

    about section

--- components/About.js
+++ components/About.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React from 'react';
 
 function About() {
-  return (
-    <div
-      id="about"
-      className=" flex items-center justify-center w-full h-screen text-9xl text-purple-500"
-    >
-      About Section
-    </div>
-  );
+	return (
+		<div
+			id="about"
+			className="flex flex-col items-center justify-center text-2xl text-gray-500"
+		>
+			<div className="text-4xl text-left border border-purple-500 sm:w-[550px] w-96 mt-12">
+				About Me
+			</div>
+			<div className="border border-white w-96 sm:w-[550px] my-12">
+				I am a self-taught developer with a focus on frontend engineering, and
+				an interest in backend as well. I come from a non-traditional
+				background. I love solving complex problems, and look forward to growing
+				in my developer career.
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- components/Project.js
+++ components/Project.js
@@ -1,42 +1,40 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
+import { FaPlay } from 'react-icons/fa';
+
+import { AiFillGithub } from 'react-icons/ai';
 function Project({ onClick, projectTitle, projectDescription }) {
-  return (
-    <div className="p-6 sm: mt-6 flex flex-col sm:flex-row items-center justify-around gap-12 bg-[#fafafa] border-b border-gray-400 w-full h-auto">
-      <a target="_blank" href="https://airbnbnextclone.vercel.app">
-        <button>
-          <Image src="/images/91374.jpg" width={600} height={500} />
-        </button>
-      </a>
-      <div className="flex flex-col gap-5 w-96 bg-[#fafafa] items-center justify-center ">
-        <div className="text-3xl font-bold text-center">{projectTitle}</div>
-        <div className="text-gray-400 text-center font-medium w-72">
-          {projectDescription}
-        </div>
-        <div className="flex justify-center gap-8">
-          {" "}
-          <button
-            onClick={onClick}
-            className="flex justify-center items-center w-32 px-5 bg-[#818cf8] transition duration-300 hover:bg-purple-500 active:scale-90 transform text-white font-medium rounded-3xl"
-          >
-            <Image src="/icons/preview-65 (1).svg" width={36} height={36} />
-            <span>Preview</span>
-          </button>
-          <a href="https://github.com/coscoaj1/airbnbnextclone" target="_blank">
-            <button className="flex justify-center items-center gap-1 w-32 px-5 py-1.5 border border-[#818cf8]  text-[#818cf8]  transition duration-300 active:scale-90 transform hover:scale-105 rounded-3xl">
-              <Image
-                src="/icons/iconmonstr-github-1 (1).svg"
-                width={36}
-                height={36}
-              />
-              GitHub
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="p-6 sm: mt-6 flex flex-col sm:flex-row items-center justify-around gap-12 bg-[#fafafa] border-b border-gray-400 w-full h-auto">
+			<a target="_blank" href="https://airbnbnextclone.vercel.app">
+				<button>
+					<Image src="/images/91374.jpg" width={600} height={500} />
+				</button>
+			</a>
+			<div className="flex flex-col gap-5 w-96 bg-[#fafafa] items-center justify-center ">
+				<div className="text-3xl font-bold text-center">{projectTitle}</div>
+				<div className="text-gray-400 text-center font-medium w-72">
+					{projectDescription}
+				</div>
+				<div className="flex justify-center gap-8">
+					{' '}
+					<button
+						onClick={onClick}
+						className="flex justify-center items-center w-32 px-5 bg-[#818cf8] transition duration-300 hover:bg-purple-500 active:scale-90 transform text-white font-medium rounded-3xl"
+					>
+						<FaPlay className="w-4 h-4" />
+						<span className="pl-2">Preview</span>
+					</button>
+					<a href="https://github.com/coscoaj1/airbnbnextclone" target="_blank">
+						<button className="flex justify-center items-center gap-1 w-32 px-5 py-1.5 border border-[#818cf8] hover:bg-[#818cf8]  text-[#818cf8] hover:text-white transition duration-300 active:scale-90 transform hover:scale-105 rounded-3xl">
+							<AiFillGithub className="w-8 h-8 pr-1" /> GitHub
+						</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Project;
