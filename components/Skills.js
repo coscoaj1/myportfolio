4ce52defb4e339b34ba9b2145d5f70a5f15e6f5f
coscoaj1 <coscoanthony1@gmail.com>
2021-11-25 14:49:12 -0500

    added alt tags to icons

--- components/Skills.js
+++ components/Skills.js
@@ -1,62 +1,71 @@
-import React from 'react';
-import Image from 'next/image';
-import { FaCode } from 'react-icons/fa';
-import { FaGit } from 'react-icons/fa';
+import React from "react";
+import Image from "next/image";
+import { FaCode } from "react-icons/fa";
+import { FaGit } from "react-icons/fa";
 
 function Skills() {
-	return (
-		<div className="relative w-screen py-6">
-			<div>
-				<div className="flex flex-col items-center justify-center w-full gap-2">
-					<p className="z-10 w-full text-5xl text-center font-bold text-[#828cf8]">
-						Skills
-					</p>
-					<FaCode className="w-20 h-20 text-gray-300" />
-				</div>
-			</div>
-			<div className="flex flex-col flex-wrap items-center justify-center w-full h-auto gap-8 mt-8 mb-24 md:flex-row max-w-screen sm:w-full">
-				<div className="z-10 flex justify-around py-2 text-left border border-gray-300 p-9 h-60 rounded-xl mx-4 w-80 sm:w-96">
-					<Image
-						src="/icons/12340106921551942641.svg"
-						width={110}
-						height={125}
-					/>
-					<Image
-						src="/icons/8905768601551941711 (1).svg"
-						width={130}
-						height={145}
-						className="text-gray-300 fill-current"
-					/>
-				</div>
-				<div className="grid border border-gray-300 place-items-center sm:w-96 w-80 h-72 rounded-xl">
-					<div className="sm:p-4">
-						<Image
-							src="/icons/PinClipart.com_eclipse-swt-plugin_3368555.png"
-							width={310}
-							height={150}
-						/>
-					</div>
-				</div>
-				<div>
-					<div className="flex flex-col items-center justify-around mx-4 border border-gray-300 w-80 sm:w-96 h-96 rounded-xl">
-						<div className="pt-8">
-							<Image
-								src="/icons/node-js-icon-11.jpg"
-								width={130}
-								height={130}
-							/>
-						</div>
-						<div>
-							<Image src="/icons/mongodb.svg" width={232} height={100} />
-						</div>
-						<div>
-							<FaGit className="w-16 h-16 text-gray-300" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative w-screen py-6">
+      <div>
+        <div className="flex flex-col items-center justify-center w-full gap-2">
+          <p className="z-10 w-full text-5xl text-center font-bold text-[#828cf8]">
+            Skills
+          </p>
+          <FaCode className="w-20 h-20 text-gray-300" />
+        </div>
+      </div>
+      <div className="flex flex-col flex-wrap items-center justify-center w-full h-auto gap-8 mt-8 mb-24 md:flex-row max-w-screen sm:w-full">
+        <div className="z-10 flex justify-around py-2 pr-1 text-left border border-gray-300 p-9 h-60 rounded-xl w-80 sm:w-96">
+          <Image
+            src="/icons/12340106921551942641.svg"
+            width={110}
+            height={125}
+            alt="React Icon"
+          />
+          <Image
+            src="/icons/8905768601551941711 (1).svg"
+            width={130}
+            height={145}
+            className="text-gray-300 fill-current"
+            alt="Next.js Icon"
+          />
+        </div>
+        <div className="grid border border-gray-300 place-items-center sm:w-96 w-80 h-72 rounded-xl">
+          <div className="sm:p-4">
+            <Image
+              src="/icons/PinClipart.com_eclipse-swt-plugin_3368555.png"
+              width={310}
+              height={150}
+              alt="html css javascript icon"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-around mx-4 border border-gray-300 w-80 sm:w-96 h-96 rounded-xl">
+            <div className="pt-8">
+              <Image
+                src="/icons/node-js-icon-11.jpg"
+                width={130}
+                height={130}
+                alt="node.js icon"
+              />
+            </div>
+            <div>
+              <Image
+                src="/icons/mongodb.svg"
+                width={232}
+                height={100}
+                alt="mongodb icon"
+              />
+            </div>
+            <div>
+              <FaGit className="w-16 h-16 text-gray-300" alt="Git icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
