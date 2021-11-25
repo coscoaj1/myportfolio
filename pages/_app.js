4868eba5e0240a3a41915e45e9d9c9d3517d9a4e
coscoaj1 <coscoanthony1@gmail.com>
2021-11-25 16:59:14 -0500

    app.js head

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,31 @@
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.css';
-import Footer from '../components/Footer';
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+import Footer from "../components/Footer";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-	const { pathname } = useRouter();
+  const { pathname } = useRouter();
 
-	useEffect(() => {
-		setTimeout(() => {
-			window.scrollTo(0, 0);
-		}, 50);
-	}, [pathname]);
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 50);
+  }, [pathname]);
 
-	return (
-		<>
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Anthony Cosco Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
